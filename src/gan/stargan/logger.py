7d3346f9ffd0efdcc3b2fca0ddd6d9fc11883fcd
KeisukeShimokawa <shimoke4869@gmail.com>
2020-05-10 12:50:11 +0900

    aff pytorch tensorboard logger

---
+++ src/gan/stargan/logger.py
@@ -0,0 +1,10 @@
+from torch.utils.tensorboard import SummaryWriter
+
+
+class Logger(object):
+
+    def __init__(self, log_dir):
+        self.writer = SummaryWriter(log_dir)
+
+    def scalar_summary(self, tag, value, step):
+        self.writer.add_scalar(tag, value, step)
