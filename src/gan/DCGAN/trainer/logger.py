7190ecd5ec02affaf8f7443b4fbd2c30ad39f539
KeisukeShimokawa <shimoke4869@gmail.com>
2020-05-19 22:28:24 +0000

    fix & test logger setup func

--- src/gan/DCGAN/trainer/logger.py
+++ src/gan/DCGAN/trainer/logger.py
@@ -2,35 +2,45 @@ from datetime import datetime
 from pathlib import Path
 from logging import getLogger, Formatter, INFO, FileHandler, StreamHandler
 from torch.utils.tensorboard import SummaryWriter
+from torchvision.utils import make_grid
 
 
 class Logger:
-    def __init__(self, log_dir, flush_secs):
+    def __init__(self, log_dir, flush_secs=30):
 
         self.log_dir = Path(log_dir)
         self.flush_secs = flush_secs
-        self.logger = self._build_logger()
-        self.timestamp = self._get_timestamp()
+        self.timestamp = None
+        self.logger = None
         self.writers = {}
+
+        self._setup_logger()
+
+    def _setup_logger(self):
+        self.timestamp = self._get_timestamp()
+        logfile_dir = self.log_dir.joinpath(self.timestamp)
+        logfile_dir.mkdir(parents=True, exist_ok=True)
+        logfile_path = logfile_dir.joinpath("logs.txt")
+        self.logger = self._build_logger(logfile_path)
+        self.print_log(f"log directory is {logfile_dir}")
 
     def _get_timestamp(self):
         return datetime.now().strftime("%Y-%m-%d_%H-%M-%H")
 
-    def _build_logger(self):
-        
-        logger = logging.getLogger(__name__)
-        logger.setLevel(logging.INFO)
+    def _build_logger(self, path):
+
+        logger = getLogger(__name__)
+        logger.setLevel(INFO)
         # format
         fmt_str = "[%(levelname)s] %(asctime)s: \t%(message)s"
-        fmt = logging.Formatter(fmt_str, "%Y-%m-%d %H-%M-%S")
+        fmt = Formatter(fmt_str, "%Y-%m-%d %H-%M-%S")
         # stream handler
-        stm_handler = logging.StreamHandler(sys.stdout)
-        stm_handler.setLevel(logging.INFO)
+        stm_handler = StreamHandler()
+        stm_handler.setLevel(INFO)
         stm_handler.setFormatter(fmt)
         # file handler
-        path = self.log_dir.joinpath(self.timestamp, "logs.txt")
-        file_handler = logging.FileHandler(path)
-        file_handler.setLevel(logging.INFO)
+        file_handler = FileHandler(path)
+        file_handler.setLevel(INFO)
         file_handler.setFormatter(fmt)
         # add handler to logger
         logger.addHandler(stm_handler)
@@ -44,7 +54,7 @@ class Logger:
     def _build_writer(self, metrics):
         writer = SummaryWriter(
             log_dir=self.log_dir.joinpath(self.timestamp, metrics),
-            flush_secs=self.flush_secs
+            flush_secs=self.flush_secs,
         )
         return writer
 
@@ -73,3 +83,8 @@ class Logger:
     def close_writers(self):
         for metrics in self.writers:
             self.writers[metrics].close()
+
+
+if __name__ == "__main__":
+    logger = Logger("./logs")
+    logger.add_scalar_log("sample", 1.0, global_step=1)
