9007984f58a537cee0a8bda826f3cafd78935a91
KeisukeShimokawa <shimoke4869@gmail.com>
2020-05-24 13:36:43 +0000

    add random seed function

---
+++ src/gan/FQGAN/training/utils.py
@@ -0,0 +1,12 @@
+import os
+import random
+import numpy as np
+import torch
+
+
+def set_seed(seed):
+    random.seed(seed)
+    np.random.seed(seed)
+    os.environ["PYTHONHASHSEED"] = str(seed)
+    torch.manual_seed(seed)
+    torch.cuda.manual_seed(seed)
