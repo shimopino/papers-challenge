f1c30a41ca5b220fa4a45ce64faf7a16772fb9f6
KeisukeShimokawa <shimoke4869@gmail.com>
2020-05-24 13:24:31 +0000

    add configuration

---
+++ src/gan/FQGAN/config/config.py
@@ -0,0 +1,59 @@
+import dataclasses
+import torch
+
+
+@dataclasses.dataclass
+class Config:
+    # general settings
+    seed: int = 42
+    ngpu: int = 1
+
+    # dataset
+    datapath: str = "data/"
+    num_workers: int = 2
+    image_size: int = 64
+
+    # model
+    nc: int = 3
+    nz: int = 128
+    ngf: int = 768
+    ndf: int = 768
+    use_sn: bool = True
+    use_cbm: bool = True
+    use_vq: bool = False
+    dict_size: int = 5  # 2^10 --> 1024
+    bottom_width: int = 4
+
+    # training
+    n_epochs: int = 30
+    batch_size: int = 128
+    beta1: float = 0.0
+    beta2: float = 0.9
+    lrD: float = 1e-4
+    lrG: float = 1e-4
+    is_ortho: bool = False
+    factor: float = 1e-4
+    loss_type: str = "bce"
+
+    # logging
+    log_dir: str = "logs"
+    flush_secs: int = 5
+    print_iters: int = 200
+
+    @property
+    def betas(self):
+        return (self.beta1, self.beta2)
+
+    @property
+    def is_cuda(self):
+        return self.ngpu > 0 and torch.cuda.is_available()
+
+    @property
+    def device(self):
+        return torch.device("cuda:0" if self.is_cuda else "cpu")
+
+    def __repr__(self):
+        msg = "\n"
+        for key, value in self.__dict__.items():
+            msg += f"{key}: {value}\n"
+        return msg
