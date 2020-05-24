32b4608fa2ef6e416904dd952265ece33bfef26f
KeisukeShimokawa <shimoke4869@gmail.com>
2020-05-24 21:57:47 +0000

    fix loss func arguments

--- src/gan/FQGAN/models/losses.py
+++ src/gan/FQGAN/models/losses.py
@@ -3,13 +3,13 @@ import torch.nn as nn
 
 
 class ProbLoss(nn.Module):
-    def __init__(self, cfg):
-        assert cfg.loss_type in ["bce", "hinge"]
+    def __init__(self, loss_type, batch_size, device):
+        assert loss_type in ["bce", "hinge"]
         super().__init__()
-        self.loss_type = cfg.loss_type
-        self.device = cfg.device
-        self.ones = torch.ones(cfg.batch_size).to(cfg.device)
-        self.zeros = torch.zeros(cfg.batch_size).to(cfg.device)
+        self.loss_type = loss_type
+        self.device = device
+        self.ones = torch.ones(batch_size).to(device)
+        self.zeros = torch.zeros(batch_size).to(device)
         self.bce = nn.BCEWithLogitsLoss()
 
     def __call__(self, logits, condition):
@@ -31,3 +31,15 @@ class ProbLoss(nn.Module):
             else:
                 minval = torch.min(-logits - 1, self.zeros[:batch_len])
                 return -torch.mean(minval)
+
+
+def ortho_reg(model, factor=1e-4, device=None):
+    with torch.enable_grad():
+        ortho_loss = torch.tensor(0.0, device=device)
+        for name, param in model.named_parameters():
+            if "bias" not in name:
+                w = param.view(param.size(0), -1)
+                sym = (w @ w.t()) * (1.0 - torch.eye(w.size(0), device=device))
+                ortho_loss += factor * sym.abs().sum()
+
+    return ortho_loss
