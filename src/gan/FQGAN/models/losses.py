3c35c0e2d3454e13660106d7f18153039dba1f30
KeisukeShimokawa <shimoke4869@gmail.com>
2020-05-27 14:19:34 +0000

    change orthogonal regularization to change model parameter not adding loss

--- src/gan/FQGAN/models/losses.py
+++ src/gan/FQGAN/models/losses.py
@@ -33,13 +33,23 @@ class ProbLoss(nn.Module):
                 return -torch.mean(minval)
 
 
-def ortho_reg(model, factor=1e-4, device=None):
-    with torch.enable_grad():
-        ortho_loss = torch.tensor(0.0, device=device)
-        for name, param in model.named_parameters():
-            if "bias" not in name:
-                w = param.view(param.size(0), -1)
-                sym = (w @ w.t()) * (1.0 - torch.eye(w.size(0), device=device))
-                ortho_loss += factor * sym.abs().sum()
+def ortho_reg(model, strength=1e-4, blacklist=None):
+    """
+    Apply Orthogonal Regularization after calculating gradient using loss backward().
 
-    return ortho_loss
+    Args:
+        model (nn.Module): nn.Module Model after loss backward
+        strength (float, optional): parameter for strengthing the effect of this regularization. Defaults to 1e-4.
+        blacklist (list, optional): set to avoid to regulate shared Generator layers. Defaults to None.
+    """
+
+    with torch.no_grad():
+        for param in model.parameters():
+            # Only apply this to parameters with at least 2 axes, and not in the blacklist
+            if len(param.shape) < 2 or any([param is item for item in blacklist]):
+                continue
+            w = param.view(param.shape[0], -1)
+            grad = 2 * torch.mm(
+                torch.mm(w, w.t()) * (1.0 - torch.eye(w.shape[0], device=w.device)), w
+            )
+            param.grad.data += strength * grad.view(param.shape)
