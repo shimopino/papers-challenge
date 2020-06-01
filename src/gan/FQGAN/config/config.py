a8fa66b68c9d5ac085f7e0ffda88811aeee84b60
KeisukeShimokawa <shimoke4869@gmail.com>
2020-06-01 13:53:44 +0000

    add configure based on torch_mimicry

--- src/gan/FQGAN/config/config.py
+++ src/gan/FQGAN/config/config.py
@@ -1,69 +1,40 @@
 from dataclasses import dataclass, field
 from typing import List
-import torch
 
 
 @dataclass
 class Config:
-    # general settings
-    seed: int = 42
-    ngpu: int = 0
-    amp: bool = False
-    opt_level: str = "O1"
-
     # dataset
-    datapath: str = "../data/celeba"
-    num_workers: int = 2
-    image_size: int = 64
+    dataset_dir: str = "./datasets"
+    dataset_name: str = "cifar10"
+    num_workers: int = 4
 
     # model
-    nc: int = 3
-    nz: int = 128
-    ngf: int = 768
-    ndf: int = 768
-    bottom_width: int = 4
-    use_sn: bool = True
-    use_cbm: bool = True
-    vq_type: str = None
-    dict_size: int = 10  # 2^10 --> 1024
+    loss_type: str = "gan"  # "hinge", "ns", "gan", "wasserstein"
+    vq_type: str = "Normal"
+    dict_size: int = 10
     quant_layers: List = field(default_factory=lambda: [3])
 
+    # optimizer
+    lrD: float = 2e-4
+    lrG: float = 2e-4
+    beta1: float = 0.0
+    beta2: float = 0.9
+
     # training
-    n_epochs: int = 30
-    batch_size: int = 128
-    beta1: float = 0.5
-    beta2: float = 0.999
-    lrD: float = 1e-4
-    lrG: float = 1e-4
-    is_ortho: bool = False
-    factor: float = 1e-4
-    loss_type: str = "bce"
+    batch_size: int = 64
+    n_dis: int = 4
+    num_steps: int = 100000
+
+    # evaluation
+    metric: str = 'fid'
+    num_real_samples: int = 10000
+    num_fake_samples: int = 10000
+    evaluate_step: int = 100000
 
     # logging
-    log_dir: str = "logs"
-    flush_secs: int = 5
-    print_iters: int = 200
-    output_dir: str = "output"
+    log_dir: str = "./logs/fqgan"
 
     @property
     def betas(self):
         return (self.beta1, self.beta2)
-
-    @property
-    def is_cuda(self):
-        return self.ngpu > 0 and torch.cuda.is_available()
-
-    @property
-    def device(self):
-        return torch.device("cuda:0" if self.is_cuda else "cpu")
-
-    def __repr__(self):
-        msg = "\n"
-        for key, value in self.__dict__.items():
-            msg += f"{key}: {value}\n"
-        return msg
-
-
-if __name__ == "__main__":
-    cfg = Config()
-    print(cfg)
